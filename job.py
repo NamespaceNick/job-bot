2969d802442f1afc9bad0c1d37e4699f80a730bd
Nicolas Williams <ndwill@umich.edu>
2020-11-14 02:06:12 -0600

    Add basic high level comments & install packages

---
+++ job.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# job.py
+# Main script
+import requests
+import logging
+
+from dotenv import load_dotenv
+
+##############################################################################
+############################## CONSTANTS & SUCH ##############################
+##############################################################################
+load_dotenv()
+
+##############################################################################
+############################ FUNCTION DEFINITIONS ############################
+##############################################################################
+
+
+##############################################################################
+################################# SCRIPT BODY ################################
+##############################################################################
+
+# TODO: Implement this section
+# Get webpage objects
+# - contain url, div keyword, company
+
+
+# TODO: Implement this section
+# Get request for html page
+
+
+# TODO: Implement this section
+# Parse webpage contents
+
+
+# TODO: Implement this section
+# Generate JobPosting objects
+
+
+# TODO: Implement this section
+# Compare to stored postings (identify new & outdated/filled postings)
+
+
+# TODO: Implement this section
+# Store job postings
+# - google sheets? webpage? unclear.
+
+
+# TODO: Implement this section
+# Send notifications about new postings
+
+
+# TODO: Error Handling
+# - process interruptions
+# - html page changes
+# - error log
